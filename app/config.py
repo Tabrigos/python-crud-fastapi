de921ae14fc426d01dd16f25df0838e58380d6ad
P.Guer <97910841+Tabrigos@users.noreply.github.com>
2023-03-14 22:57:41 +0100

    Update config.py
    secret fix

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = 'DATABASE_CONNECTION'
+DATABASE_URL = 'secret.DATABASE_CONNECTION'
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
