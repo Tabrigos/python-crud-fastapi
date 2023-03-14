43ca1b869a28c918188d2a5e8441dfd71860e2fe
P.Guer <97910841+Tabrigos@users.noreply.github.com>
2023-03-14 17:22:14 +0100

    Update config.py
    using secrets

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = 'postgresql://user:admin@localhost:5432/user'
+DATABASE_URL = ''
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
